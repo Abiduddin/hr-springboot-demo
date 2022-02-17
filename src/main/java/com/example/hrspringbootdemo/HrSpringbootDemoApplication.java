55916702558ce466e1ad3e8bb6564738e044a2ad
Abiduddin <abidakash456@gmail.com>
2022-02-17 16:44:25 +0600

    project init

---
+++ src/main/java/com/example/hrspringbootdemo/HrSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hrspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrSpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrSpringbootDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hrspringbootdemo/HrSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hrspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrSpringbootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
